b06881b9ef6e16fbe3e0c80cf808abc85125d2d9
Franklyn Dunbar <franklyn.dunbar@umontana.edu>
2019-02-04 13:10:29 -0700

    Added sobel filter and its transpose

--- test.py
+++ test.py
@@ -10,8 +10,8 @@ I_gray = img_color.mean(axis=2)
 I_gray_copy = img_color.mean(axis=2)
 h_box = np.array([[1/9,1/9,1/9],[1/9,1/9,1/9],[1/9,1/9,1/9]])
 h_gauss = np.array([[1/9,4/9,1/9],[4/9,1/9,4/9],[1/9,4/9,1/9]])
-              
-# g @ h = A 
+h_sobel = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])              
+
 def convolve(g,h): # h is kernel, g is the image
     start = 1
     for i in range(start,len(g[:,1])-1):
@@ -20,9 +20,7 @@ def convolve(g,h): # h is kernel, g is the image
             total = h*f
             I_gray_copy[i][j] = sum(sum(total)) 
     return I_gray_copy
-            # get 3x3 neighborhood of g then multiply it by h
-            # call kernel to get the average pixel value and set 
-
+           
 def gauss_kernal(size, var):
     kernel = np.zeros(shape=(size,size))
     for i in range(size):
@@ -30,14 +28,18 @@ def gauss_kernal(size, var):
             kernel[i][j] = mt.exp( -((i - (size-1)/2)**2 + (j - (size-1)/2)**2 )/(2*var*var))
     return kernel           
 
-    # Return the ave pixel value
+    
 I_gray_copy_box = convolve(I_gray, h_box)
 I_gray_copy_gauss = convolve(I_gray, h_gauss)
 I_gray_gauss_calc = convolve(I_gray,gauss_kernal(3,2))
+I_gray_sobel = convolve(I_gray,h_sobel)
+I_gray_sobel_trans = convolve(I_gray,h_sobel.Transpose())
 
 im.imwrite("box.jpg", I_gray_copy_box)
 im.imwrite("gauss_given.jpg", I_gray_copy_gauss)
 im.imwrite("gauss_calc.jpg", I_gray_gauss_calc)
+im.imwrite("sobel.jpg",I_gray_sobel)
+im.imwrite("sobel_trans.jpg",I_gray_sobel_trans)
 
 print( " test.py is done ")
 
